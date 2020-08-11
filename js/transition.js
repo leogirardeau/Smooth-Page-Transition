14c35c3fbe85a61f28a0bdf9efc0da2122e16f40
leogirardeau <leogir.pro@gmail.com>
2020-08-11 15:49:17 +0200

    adding function/exporting/spacing

--- js/transition.js
+++ js/transition.js
@@ -5,12 +5,21 @@ import { TimelineLite } from 'gsap/dist/gsap';
 
 class Fade extends Highway.Transition {
     in({ from, to, done }) {
-
         const t1 = new TimelineLite();
-        t1.fromTo(to, 0.5, { left: '-100%', top: '50%' }, { left: '0%' }).fromTo(to, 0.5, { height: '2vh' }, { height: '90vh', top: '10%' });
-
+        t1.fromTo(to, 0.5, { left: '-100%', top: '50%' }, { left: '0%' }).fromTo(
+            to,
+            0.5,
+            { height: '2vh' },
+            {
+                height: '90vh', top: '10%', onComplete: function () {
+                    done();
+                }
+            }
+        );
     }
     out({ from, done }) {
         done();
     }
 }
+
+export default Fade;
